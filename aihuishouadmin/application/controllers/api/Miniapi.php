4a54ea3442f10892c456938a017968b906e91b6f
Gary <zhaoyue@dlhzxt.cn>
2022-06-07 07:38:54 +0800

    微信打垮

--- aihuishouadmin/application/controllers/api/Miniapi.php
+++ aihuishouadmin/application/controllers/api/Miniapi.php
@@ -1483,7 +1483,129 @@ class Miniapi extends CI_Controller
 		$membernewmoney = floatval($sum_price) + floatval($MemberInfomid['wallet']);
 		$this->mini->member_edit_wallet($mid,$membernewmoney);
 
-		$this->back_json(200, '操作成功');
+		$re_openid = $MemberInfomid['openid'];
+		$total_amount = (100) * $sum_price;
+
+		$data=array(
+			'mch_appid'=>'wx8be053f4e70ec431',                            //商户账号appid
+			'mchid'=> '1622453928',                                       //商户号
+			'nonce_str'=>$this->createNoncestr(),                         //随机字符串
+			'partner_trade_no'=> date('YmdHis').rand(1000, 9999),  //商户订单号
+			'openid'=> $re_openid,                                        //用户openid
+			'check_name'=>'NO_CHECK',                                     //校验用户姓名选项,
+			'amount'=>$total_amount,                                      //金额
+			'desc'=> "用户提现",                                           //企业付款描述信息
+		);
+
+		//生成签名算法
+		$secrect_key="Aihuishou12345678987654321234567";                  //这个就是个API密码。
+		$data=array_filter($data);
+		ksort($data);
+		$str='';
+		foreach($data as $k=>$v) {
+			$str.=$k.'='.$v.'&';
+		}
+		$str.='key='.$secrect_key;
+		$data['sign']=md5($str);
+		//生成签名算法
+
+		$xml=$this->arraytoxml($data);
+		$url='https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers'; //调用接口
+		$res=$this->curl_post_ssl($url,$xml);
+		$return=$this->xmltoarray($res);
+		$res = $return['result_code'];
+//        $res = 'SUCCESS';
+//        $responseObj = simplexml_load_string($res, 'SimpleXMLElement', LIBXML_NOCDATA);
+//        $res= $responseObj->result_code; //SUCCESS 如果返回来SUCCESS,则发生成功，处理自己的逻辑
+		if ($res === 'SUCCESS'){
+			$this->back_json(200, '操作成功');
+		}else{
+			$this->back_json(205, '操作失败,您的商户余额不足！');
+		}
+	}
+
+	/**
+	 * [xmltoarray xml格式转换为数组]
+	 * @param [type] $xml [xml]
+	 * @return [type]  [xml 转化为array]
+	 */
+	function xmltoarray($xml) {
+		//禁止引用外部xml实体
+		libxml_disable_entity_loader(true);
+		$xmlstring = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+		$val = json_decode(json_encode($xmlstring),true);
+		return $val;
+	}
+
+	/**
+	 * [arraytoxml 将数组转换成xml格式（简单方法）:]
+	 * @param [type] $data [数组]
+	 * @return [type]  [array 转 xml]
+	 */
+	function arraytoxml($data){
+		$str='<xml>';
+		foreach($data as $k=>$v) {
+			$str.='<'.$k.'>'.$v.'</'.$k.'>';
+		}
+		$str.='</xml>';
+		return $str;
+	}
+
+	/**
+	 * [createNoncestr 生成随机字符串]
+	 * @param integer $length [长度]
+	 * @return [type]   [字母大小写加数字]
+	 */
+	function createNoncestr($length =32){
+		$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789";
+		$str ="";
+
+		for($i=0;$i<$length;$i++){
+			$str.= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+		}
+		return $str;
+	}
+
+	/**
+	 * [curl_post_ssl 发送curl_post数据]
+	 * @param [type] $url  [发送地址]
+	 * @param [type] $xmldata [发送文件格式]
+	 * @param [type] $second [设置执行最长秒数]
+	 * @param [type] $aHeader [设置头部]
+	 * @return [type]   [description]
+	 */
+	function curl_post_ssl($url, $xmldata, $second = 30, $aHeader = array()){
+		$isdir = $_SERVER['DOCUMENT_ROOT']."/cert/";//证书位置;绝对路径
+
+		$ch = curl_init();//初始化curl
+
+		curl_setopt($ch, CURLOPT_TIMEOUT, $second);//设置执行最长秒数
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+		curl_setopt($ch, CURLOPT_URL, $url);//抓取指定网页
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);// 终止从服务端进行验证
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);//
+		curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');//证书类型
+		curl_setopt($ch, CURLOPT_SSLCERT, $isdir . 'apiclient_cert.pem');//证书位置
+		curl_setopt($ch, CURLOPT_SSLKEYTYPE, 'PEM');//CURLOPT_SSLKEY中规定的私钥的加密类型
+		curl_setopt($ch, CURLOPT_SSLKEY, $isdir . 'apiclient_key.pem');//证书位置
+		curl_setopt($ch, CURLOPT_CAINFO, 'PEM');
+		curl_setopt($ch, CURLOPT_CAINFO, $isdir . 'rootca.pem');
+		if (count($aHeader) >= 1) {
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $aHeader);//设置头部
+		}
+		curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $xmldata);//全部数据使用HTTP协议中的"POST"操作来发送
+
+		$data = curl_exec($ch);//执行回话
+		if ($data) {
+			curl_close($ch);
+			return $data;
+		} else {
+			$error = curl_errno($ch);
+			echo "call faild, errorCode:$error\n";
+			curl_close($ch);
+			return false;
+		}
 	}
 
 	public function order_update_goods(){
